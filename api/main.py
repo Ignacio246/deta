ea0d6a00ebc3b3711c451cd1ec2f4386aaff22c0
Ignacio246 <1718110989@utectulancingo.edu.mx>
2022-12-02 21:12:31 +0000

    Rentals

--- api/main.py
+++ api/main.py
@@ -1,20 +1,53 @@
 from fastapi import FastAPI
-import pickle
+from fastapi import status
+from pydantic import BaseModel
+from fastapi.responses import JSONResponse
+from joblib import load
 
-app = FastAPI()
+class Features(BaseModel):
+    season: int
+    mnth: int
+    holiday: int
+    weekday: int
+    workingday: int
+    weathersit: int
+    temp: float
+    hum: float
+    windspeed: float
+
+    class Config:
+        schema_extra = {
+            "example":{
+                "season": 3.,
+                "mnth":7.,
+                "holiday": 0.,
+                "weekday": 6.,
+                "workingday": 1,
+                "temp": 0.686667,
+                "atemp": 0.638263,
+                "hum": 0.585,
+                "windspeed": 0.208342
+            }
+        }
+class Label(BaseModel):
+    rentals: float
+
+class Message(BaseModel):
+    message: float
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+app = FastAPI(
+    title="Rents for FastAPI",
+    description=description,
+    version="1.0.0",
+    terms_of_service=""
+)
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int):
-    return {"item_id": item_id}
-
-
-filename = "model.save"
-model = pickle.load(open(filename,'rb'))
-predictions = model.predict([[datos]])
-
+@app.post("/rentals",
+response_model= Label,
+status_code=status.HTTP_200_OK,
+summary="Rentals",
+description="Rentals",
+tags=["Rentals"]
+)
+async def get_rentals
